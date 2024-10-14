3c6fee3f6a108baf49384f072fbc2f9abbf1106c
Charis <chvlatakis@gmail.com>
2024-10-14 21:04:30 +0300

    AgainChanged

--- src/gr/aueb/cf/Hello2.java
+++ src/gr/aueb/cf/Hello2.java
@@ -2,6 +2,6 @@ package gr.aueb.cf;
 
 public class Hello2 {
     public static void main(String[] args) {
-        System.out.println("Hello Again");
+        System.out.println("Hello Again and Again!!");
     }
 }
